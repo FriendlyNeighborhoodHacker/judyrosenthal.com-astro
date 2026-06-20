cfa35b1f4112116382c9cf44238d7d8f9df236c9
Lilly Rosenthal <lillyjrosenthal123@gmail.com>
2026-06-20 11:53:29 -0400

    combine_art_slides_and_slides

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -67,6 +67,90 @@ export const slides: Slide[] = [
     titleStyle: "label"
   },
   {
+    collection: "bicentennial",
+    title: "Bicentennial Project",
+    crumbs: [
+      { label: "Home", href: "/" },
+      { label: "Art", href: "/art/" },
+      { label: "Bicentennial Project" }
+    ],
+    link: "/art/bicentennial-project/",
+    landscape: "images/art/art_bicentennial_landscape.png",
+    portrait: "images/art/art_bicentennial_portrait.png",
+    menuColor: "white",
+    shadowColor: "rgba(0,0,0,0.75)"
+  },
+  {
+    collection: "houston",
+    title: "Houston",
+    crumbs: [
+      { label: "Home", href: "/" },
+      { label: "Art", href: "/art/" },
+      { label: "Houston" }
+    ],
+    link: "/art/houston/",
+    landscape: "images/art/art_houston_landscape.png",
+    portrait: "images/art/art_houston_portrait.png",
+    menuColor: "white",
+    shadowColor: "rgba(0,0,0,0.75)"
+  },
+  {
+    collection: "landscapes",
+    title: "Landscapes",
+    crumbs: [
+      { label: "Home", href: "/" },
+      { label: "Art", href: "/art/" },
+      { label: "Landscapes" }
+    ],
+    link: "/art/landscapes/",
+    landscape: "images/art/art_landscapes_landscape.png",
+    portrait: "images/art/art_landscapes_portrait.png",
+    menuColor: "white",
+    shadowColor: "rgba(0,0,0,0.75)"
+  },
+  {
+    collection: "patriotic",
+    title: "Patriotic",
+    crumbs: [
+      { label: "Home", href: "/" },
+      { label: "Art", href: "/art/" },
+      { label: "Patriotic" }
+    ],
+    link: "/art/patriotic/",
+    landscape: "images/art/art_patriotic_landscape.png",
+    portrait: "images/art/art_patriotic_portrait.png",
+    menuColor: "white",
+    shadowColor: "rgba(0,0,0,0.75)"
+  },
+  {
+    collection: "seascapes",
+    title: "Seascapes",
+    crumbs: [
+      { label: "Home", href: "/" },
+      { label: "Art", href: "/art/" },
+      { label: "Seascapes" }
+    ],
+    link: "/art/seascapes/",
+    landscape: "images/art/art_seascapes_landscape.png",
+    portrait: "images/art/art_seascapes_portrait.png",
+    menuColor: "white",
+    shadowColor: "rgba(0,0,0,0.75)"
+  },
+  {
+    collection: "abstract_expressionism",
+    title: "Abstract Expressionism",
+    crumbs: [
+      { label: "Home", href: "/" },
+      { label: "Art", href: "/art/" },
+      { label: "Abstract Expressionism" }
+    ],
+    link: "/art/abstract-expressionism/",
+    landscape: "images/art/art_abstract_expressionism_landscape.png",
+    portrait: "images/art/art_abstract_expressionism_portrait.png",
+    menuColor: "white",
+    shadowColor: "rgba(0,0,0,0.75)"
+  }, 
+  {
     image: "images/homepage/02_The_Laura_full.jpg",
     title: "Art > Bicentennial Project",
     crumbs: [{ label: "Art", href: "/art" }, { label: "Bicentennial Project" }],
